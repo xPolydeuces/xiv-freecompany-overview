c82cbadfb0d8ba50c6887a5faadde6b732cf038e
Bartosz Kot <bartosz.kot.00@gmail.com>
2023-01-28 15:20:00 +0100

    Create useAxios hook

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,1 +1,14 @@
 // Import data from API using Axios
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const useAxios = ({ CONTENT_URL }) => {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get(CONTENT_URL).then(response => {setData(response.data)})
+  }, [])
+  return data
+}
+
+export default useAxios
